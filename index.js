1c73c30fb699ceaf6c221373576c956f7164056d
Mark Baker <mrbaker1917@gmail.com>
2018-07-19 15:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let arr1 = [];
+  for (var i = 0; i < musicians.length; i++) {
+    arr1.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr1;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+let hardFacts = [];
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    hardFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  console.log(hardFacts);
+  return hardFacts;
+}
